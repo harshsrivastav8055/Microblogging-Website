551336cbbe6e54374095753e227f7a526700a11e
harshsrivastav8055 <srivastavaharsh8055@gmail.com>
2025-07-10 11:13:36 +0530

    fourth

--- frontend/twitterclone/src/redux/utils/constant.js
+++ frontend/twitterclone/src/redux/utils/constant.js
@@ -1,5 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+// export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+// export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+
+export const USER_API_END_POINT = "https://microblogging-website-fbce.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT = "https://microblogging-website-fbce.vercel.app/api/v1/tweet";
+
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
